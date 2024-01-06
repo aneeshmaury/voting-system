1791cae62ed39e0d9080294d3055c89cf27d0474
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-01-06 19:26:47 +0530

    add loader

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 var ageInput = document.getElementById('ageInput');
 var outputBox = document.getElementById('outputBox');
 var Btn=document.getElementById('btn');
+var hideWala=document.getElementById('hidewala');
 
 Btn.addEventListener("click",function(){
-display()
+   setTimeout(function(){
+      document.getElementById('hidewala').style.display = 'none';
+      display()
+   },5000)
 
 })
+
+
 
 function display(){
 
 var age=parseInt(ageInput.value)
+
 if(18<age){
-   outputBox.innerText="you are elligible becouse your age is : " +age;
+   outputBox.innerText=" YOU ARE ELIGIBLE :) ";
 }
 else{
-   outputBox.innerText="you are not Elligible for vote age :" +age;
+   outputBox.innerText="YOU ARE NOT ELIGIBLE :(";
 }
 
 }
+
